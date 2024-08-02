8813c3584168b15a707b95e5d7714b09af585dd4
KhristenkoE <ekhristenko0@gmail.com>
2024-08-02 22:12:49 +0200

    allow only mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
 import {redirect} from "next/navigation";
 
 export default function Home() {
-  redirect('/photo/gallery');
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return redirect('/photo/gallery');
+  }
+
+  return (
+    <h1>You may only use the app from a mobile device</h1>
+  );
 }
