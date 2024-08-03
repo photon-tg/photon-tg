78afaf2214b383dcf934b51cd304b9e34c0ce9a7
KhristenkoE <ekhristenko0@gmail.com>
2024-08-03 14:52:24 +0200

    send dataCheckString

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    console.dir(window.Telegram, 'w')
     if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent)) {
       setIsLoading(false);
       return;
@@ -16,6 +15,20 @@ export default function Home() {
 
     router.replace('/photo/gallery');
   }, [router]);
+
+  useEffect(() => {
+    const send = async () => {
+      console.dir(window.Telegram, 'w')
+      const resp = await fetch('/api/auth', {
+        method: 'POST',
+        body: JSON.stringify({
+          dataCheckString: window?.Telegram?.WebApp.initData,
+        })
+      });
+      console.log(resp, 'rr')
+    }
+send();
+  }, []);
 
   if (isLoading) {
     return <div>Loading...</div>
