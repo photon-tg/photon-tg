373c4af413dda96da2f11e69b0fb9a85bf263116
KhristenkoE <ekhristenko0@gmail.com>
2024-08-03 14:37:40 +0200

    add first version of auth route

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,50 @@
+import * as crypto from "crypto";
+import {z} from "zod";
+
+const AuthenticationData = z.object({
+  dataCheckString: z.string(),
+});
+
+type AuthenticationDataBody = z.infer<typeof AuthenticationData>;
+
+export async function POST(request: Request) {
+  const body: AuthenticationDataBody = await request.json();
+
+  const { data, error, success } = AuthenticationData.safeParse(body);
+
+  if (!success) {
+    console.log(error);
+    return new Response(JSON.stringify({
+      message: 'Error occurred while validating'
+    }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const dataCheckString = body.dataCheckString;
+    const dataCheckMap = new URLSearchParams(dataCheckString);
+    const hash = dataCheckMap.get('hash');
+    const secretKey = crypto.createHmac('sha256', process.env.TELEGRAM_BOT_TOKEN!).update('WebAppData').digest();
+    const calculatedHash = crypto.createHmac('sha256', secretKey).update(dataCheckString).digest('hex');
+
+    if (calculatedHash === hash) {
+      // data is from Telegram
+      console.log('success');
+      const userData: WebAppUser = JSON.parse(dataCheckMap.get('user') as string);
+      const response= {
+        telegramId: userData.id,
+        firstName: userData.first_name,
+        lastName: userData.last_name,
+        language: userData.language_code,
+      }
+      return new Response(JSON.stringify(response), {
+        status: 200,
+      });
+    }
+
+    return new Response(JSON.stringify({ message: 'You are not allowed to use this app outside of telegram' }), { status: 403 });
+  } catch (err) {
+    return new Response(JSON.stringify({ message: 'Internal server error' }), { status: 500 });
+  }
+}
