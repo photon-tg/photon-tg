ae9595ed526eb1210f249108473cd434c08522a3
KhristenkoE <ekhristenko0@gmail.com>
2024-09-01 18:35:29 +0200

    fix

--- src/app/api/friends/route.ts
+++ src/app/api/friends/route.ts
@@ -22,20 +22,19 @@ export async function POST(request: Request) {
 		}
 
 		const tgId = await request.json();
-
-		if (typeof userData !== 'object' || !userData.user_metadata || !tgId.telegramId) throw new Error();
-
+		console.log((typeof userData === 'string' || !userData.user_metadata) && !tgId?.telegramId, 'id')
+		if ((typeof userData === 'string' || !userData.user_metadata) && !tgId?.telegramId) throw new Error();
+		console.log('hello')
 		await supabase.auth.signInWithPassword({
 			email: 'edge-functions@photon.com',
 			password: 'redound_chapbook_HOYDEN_rye_begotten_plump_passband',
 		});
 
-		const telegramId = userData.user_metadata.telegram_id ?? tgId.telegramId;
+		const telegramId = userData.user_metadata.telegram_id ?? tgId?.telegramId;
 
 		const userReferralsResponse = await supabase.from('user_referrals')
 			.select('referral_id,is_claimed_by_referrer')
 			.eq('referrer_id', telegramId);
-
 		if (userReferralsResponse.error) {
 			throw '';
 		}
