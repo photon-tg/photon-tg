d34080d6c0ea164e7c681f303f24091d10cc92d6
KhristenkoE <ekhristenko0@gmail.com>
2024-09-01 15:16:30 +0200

    temp

--- src/graphql/queries/photo.ts
+++ src/graphql/queries/photo.ts
@@ -4,7 +4,7 @@ export const GET_USER_PHOTOS = graphql(`
 	query GetUserPhotos($userId: UUID!) {
 		user_photosCollection(
 			first: 100
-			orderBy: { created_at: AscNullsLast }
+			orderBy: { created_at: DescNullsLast }
 			filter: { user_id: { eq: $userId } }
 		) {
 			edges {
