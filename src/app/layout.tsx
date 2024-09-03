4d8925fd5c9f016fc43d92df056f6f903c1f0223
KhristenkoE <ekhristenko0@gmail.com>
2024-09-03 10:59:59 +0200

    fix gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,29 @@ export default function RootLayout({
 					src="https://telegram.org/js/telegram-web-app.js"
 					strategy={'beforeInteractive'}
 				/>
-				<GoogleAnalytics gaId="G-59KV2HBP" />
+			<Script
+				id={'gtm'}
+				strategy={'beforeInteractive'}
+				dangerouslySetInnerHTML={{
+					__html: `<!-- Google Tag Manager -->
+						<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-59KV2HBP');</script>
+						<!-- End Google Tag Manager -->`
+				}}
+			/>
+			<Script
+				id={'gtm-noscript'}
+				strategy={'beforeInteractive'}
+				dangerouslySetInnerHTML={{
+					__html: `<!-- Google Tag Manager (noscript) -->
+						<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59KV2HBP"
+						height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+						<!-- End Google Tag Manager (noscript) -->`
+				}}
+			/>
 			</body>
 		</html>
 	);
