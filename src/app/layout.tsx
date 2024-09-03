c9c2fffca564d9ad28daddbdd501d658b09121ef
KhristenkoE <ekhristenko0@gmail.com>
2024-09-03 12:04:48 +0200

    fix gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata, Viewport } from 'next';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
 import Script from 'next/script';
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google';
 
 import { ApplicationProviders } from '@/components/ApplicationProviders';
-import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Montserrat({ subsets: ['latin'] });
 
@@ -27,6 +27,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html className={inter.className} lang={'en'}>
+		<GoogleTagManager gtmId="GTM-59KV2HBP" />
+		<GoogleAnalytics gaId={'G-SQ0XS74EJ2'} />
 			<body
 				className={
 					'h-full bg-gradient-to-b from-saturated-blue from-0% to-dark-black to-100% bg-no-repeat'
@@ -37,22 +39,6 @@ export default function RootLayout({
 					src="https://telegram.org/js/telegram-web-app.js"
 					strategy={'beforeInteractive'}
 				/>
-			<Script
-				id={'gtm'}
-				strategy={'beforeInteractive'}
-			>{`
-				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','GTM-59KV2HBP');</script>`}
-			</Script>
-			<Script
-				id={'gtm-noscript'}
-			>
-				{`<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59KV2HBP"
-						height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`}
-			</Script>
 			</body>
 		</html>
 	);
