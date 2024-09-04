9dd411adf47d9016d78dcc8272da20e4f9e97c7a
KhristenkoE <ekhristenko0@gmail.com>
2024-09-04 21:06:11 +0200

    add shaodw for taparea

--- src/containers/Tap/Tap.tsx
+++ src/containers/Tap/Tap.tsx
@@ -55,11 +55,11 @@ function TapArea(props: TapAreaProps) {
 			onTouchStart={onTouchStart}
 			onTouchEnd={onTouchEnd}
 			className={
-			'select-none h-[300px] w-[300px] rounded-[50%] bg-gradient-to-b from-[#5EB3DC] from-0% via-[#11419B] via-50% to-[#0F3F99] to-100% px-[12px] py-[12px]'
+			'select-none h-[300px] w-[300px] rounded-[50%] bg-gradient-to-b from-[#5EB3DC] from-0% via-[#11419B] via-50% to-[#0F3F99] to-100% px-[12px] py-[12px] shadow-[0_0_93px_5px_rgba(8,74,199,0.45)]'
 			}
 		>
 			<div
-				className={`flex h-full w-full items-center justify-center rounded-[50%] ${isPressed ? 'bg-[#041530]' : 'bg-[#041b3e]'}`}
+				className={`flex h-full w-full items-center justify-center rounded-[50%] ${isPressed ? 'bg-[#041530]' : 'bg-[#07224c]'}`}
 			>
 				<img className={'w-[80%]'} src={'/assets/camera-big.png'} />
 			</div>
