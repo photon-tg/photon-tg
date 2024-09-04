eb99e5d301f691ffe4560ed067e0e34ed6bd9459
KhristenkoE <ekhristenko0@gmail.com>
2024-09-04 21:47:17 +0200

    try fix

--- src/containers/Tap/Tap.tsx
+++ src/containers/Tap/Tap.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useApplicationContext } from '@/contexts/ApplicationContext/ApplicationContext';
 
 export function Tap() {
 	const { coins, maxEnergy, energy, onTap } = useApplicationContext();
 
 	return (
-		<div className={'flex h-full items-center justify-center'}>
+		<div unselectable={'on'} onSelectCapture={(e) => {e.preventDefault();}} className={'flex h-full items-center justify-center'}>
 			<div
 				className={
 					'grid-rows-[repeat(3, min-content)] grid items-center justify-center gap-y-[15px]'
@@ -39,6 +39,13 @@ function TapArea(props: TapAreaProps) {
 	const { onTap } = props;
 
 	const [isPressed, setIsPressed] = useState(false);
+	const tapAreaRef = useRef<HTMLButtonElement>(null);
+
+	useEffect(() => {
+		if (tapAreaRef.current) {
+			tapAreaRef.current.onselectstart = (e) => {e.preventDefault(); return false;}
+		}
+	}, [tapAreaRef.current]);
 
 	const onTouchStart = () => {
 		window.Telegram.WebApp.HapticFeedback.impactOccurred('light');
@@ -51,18 +58,23 @@ function TapArea(props: TapAreaProps) {
 	};
 
 	return (
-		<button
-			onTouchStart={onTouchStart}
-			onTouchEnd={onTouchEnd}
-			className={
-			'select-none h-[300px] w-[300px] rounded-[50%] bg-gradient-to-b from-[#5EB3DC] from-0% via-[#11419B] via-50% to-[#0F3F99] to-100% px-[12px] py-[12px] shadow-[0_0_93px_5px_rgba(8,74,199,0.45)]'
-			}
-		>
+		<button ref={tapAreaRef} unselectable={'on'} onSelectCapture={(e) => e.preventDefault() } onSelect={(e) => {e.preventDefault()}} onTouchStart={onTouchStart}
+						onTouchEnd={onTouchEnd} className={'relative'}>
+			<div className={'top-0 left-0 absolute bg-[transparent] w-full h-full z-40'}></div>
 			<div
-				className={`flex h-full w-full items-center justify-center rounded-[50%] ${isPressed ? 'bg-[#041530]' : 'bg-[#07224c]'}`}
-			>
-				<img className={'w-[80%]'} src={'/assets/camera-big.png'} />
-			</div>
-		</button>
+				unselectable={'on'}
+					className={
+					'pointer-events-none select-none h-[300px] w-[300px] rounded-[50%] bg-gradient-to-b from-[#5EB3DC] from-0% via-[#11419B] via-50% to-[#0F3F99] to-100% px-[12px] py-[12px] shadow-[0_0_93px_5px_rgba(8,74,199,0.45)]'
+					}
+				>
+					<div
+						className={`pointer-events-none flex h-full w-full items-center justify-center rounded-[50%] ${isPressed ? 'bg-[#072047]' : 'bg-[#07224c]'}`}
+					>
+						<img unselectable={'on'} style={{
+							scale: isPressed ? '1.03' : '1',
+						}} className={'w-[80%] select-none pointer-events-none'} src={'/assets/camera-big.png'} />
+					</div>
+				</div>
+			</button>
 	);
 }
