4c89a2c07f2e9aae5f3c63c048be6755791504b7
Evgenii Khristenko <krdfehe@yandex.ru>
2024-11-10 17:27:50 +0100

    fix

--- src/app/api/manage-battles/route.ts
+++ src/app/api/manage-battles/route.ts
@@ -12,6 +12,7 @@ export const revalidate = 0;
 export async function GET(request: Request) {
 	try {
 		const authHeader = request.headers.get('authorization');
+		console.log(authHeader, 'auth')
 		if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
 			return new Response('Unauthorized', {
 				status: 401,
