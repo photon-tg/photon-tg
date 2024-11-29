6faf977d225769ffeaf0a56eb2046c7d524833c8
Evgenii Khristenko <krdfehe@yandex.ru>
2024-11-29 17:13:59 +0100

    ac

--- src/app/api/check-telegram-data/route.ts
+++ src/app/api/check-telegram-data/route.ts
@@ -2,7 +2,7 @@ import { TelegramAuth } from '@/utils/telegram-auth-verification';
 import { z } from 'zod';
 import { UserErrorType } from '@/model/user/types';
 
-export const runtime = "node"
+export const runtime = "nodejs"
 
 const AuthenticationData = z.object({
 	dataCheckString: z.string().trim().min(1),
