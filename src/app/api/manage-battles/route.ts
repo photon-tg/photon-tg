207a062bb7c3224d216e7cb015481505435976b1
Evgenii Khristenko <krdfehe@yandex.ru>
2024-12-05 00:36:32 +0100

    fix

--- src/app/api/manage-battles/route.ts
+++ src/app/api/manage-battles/route.ts
@@ -1,9 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { minutesSinceUTCDate } from '@/utils/date';
-
-const TWELVE_HOURS = 720;
-const TWENTY_FOUR_HOURS = 1440;
-const THIRTY_SIX_HOURS = 2160;
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
@@ -11,6 +6,8 @@ export const revalidate = 0;
 
 export async function GET(request: Request) {
 	try {
+		if (process.env.NODE_ENV === 'development') return new Response('No local');
+
 		const authHeader = request.headers.get('auth');
 
 		if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
@@ -24,117 +21,50 @@ export async function GET(request: Request) {
 			process.env.SUPABASE_SERVICE_ROLE_KEY!,
 		);
 
-		const { data: activeBattles, error: activeBattlesError } = await supabase
+		const { data, error } = await supabase
 			.from('battles')
 			.select('*')
 			.eq('is_active', true)
+			.in('stage', ['join', 'join_vote', 'vote'])
 			.order('start_date', { ascending: true });
 
-		if (activeBattlesError) {
-			console.error(
-				'Error fetching ACTIVE battles:',
-				activeBattlesError.message,
-			);
-			return;
+		if (error) {
+			throw new Error('Error')
 		}
 
-		const [firstBattle, secondBattle] = activeBattles || [];
+		const battles = (data as { stage: string; id: string }[]);
 
-		if (activeBattles?.length < 1) {
-			throw new Error('no active battles');
+		const joinBattle = battles?.find(({ stage }) => stage === 'join');
+		const joinAndVoteBattle = battles?.find(({ stage }) => stage === 'join_vote');
+		const voteBattle = battles?.find(({ stage }) => stage === 'vote');
+
+		if (joinBattle) {
+			await supabase
+				.from('battles')
+				.update({ stage: 'join_vote' })
+				.eq('id', joinBattle.id);
 		}
 
-		const isFirstBattle36HoursIn =
-			minutesSinceUTCDate(firstBattle.start_date) >= THIRTY_SIX_HOURS;
-		const isFirstBattle24HoursIn =
-			minutesSinceUTCDate(firstBattle.start_date) >= TWENTY_FOUR_HOURS;
-		const isFirstBattle12HoursIn =
-			minutesSinceUTCDate(firstBattle.start_date) >= TWELVE_HOURS;
+		if (joinAndVoteBattle) {
+			await supabase
+				.from('battles')
+				.update({ stage: 'vote' })
+				.eq('id', joinAndVoteBattle.id);
+		}
 
-		if (firstBattle.stage === 'vote' && isFirstBattle36HoursIn) {
-			/** Need to change stage to 'finish' */
-			const response = await supabase
+		if (voteBattle) {
+			await supabase
 				.from('battles')
 				.update({ stage: 'finish', is_active: false })
-				.eq('id', firstBattle.id);
+				.eq('id', voteBattle.id);
 
-			if (response.error) {
-				console.error(response);
-			}
-		}
-
-		if (firstBattle.stage === 'join_vote' && isFirstBattle24HoursIn) {
-			/** Need to change stage to 'vote' */
-			const response = await supabase
-				.from('battles')
-				.update({ stage: 'vote' })
-				.eq('id', firstBattle.id);
-
-			if (response.error) {
-				console.error(response);
-			}
-		}
-
-		if (firstBattle.stage === 'join' && isFirstBattle12HoursIn) {
-			/** Need to change stage to 'join_vote' */
-			const response = await supabase
-				.from('battles')
-				.update({ stage: 'join_vote' })
-				.eq('id', firstBattle.id);
-
-			if (response.error) {
-				console.error(response);
-			}
-		}
-
-		if (!secondBattle && isFirstBattle24HoursIn) {
-			/** Need to create next battle */
-			const response = await supabase
+			await supabase
 				.from('battles')
 				.insert({ stage: 'join', is_active: true });
-
-			if (response.error) {
-				console.error(response);
-			}
-
-			return new Response('ok');
 		}
 
-		if (!secondBattle) {
-			return new Response('ok');
-		}
-
-		const isSecondBattle12HoursIn =
-			minutesSinceUTCDate(secondBattle.start_date) >= TWELVE_HOURS;
-		const isSecondBattle24HoursIn =
-			minutesSinceUTCDate(secondBattle.start_date) >= TWENTY_FOUR_HOURS;
-
-		if (secondBattle.stage === 'join_vote' && isSecondBattle24HoursIn) {
-			/** Need to change stage to 'vote' */
-			const response = await supabase
-				.from('battles')
-				.update({ stage: 'vote' })
-				.eq('id', secondBattle.id);
-
-			if (response.error) {
-				console.error(response);
-			}
-		}
-
-		if (secondBattle.stage === 'join' && isSecondBattle12HoursIn) {
-			/** Need to change stage to 'join_vote' */
-			const response = await supabase
-				.from('battles')
-				.update({ stage: 'join_vote' })
-				.eq('id', secondBattle.id);
-
-			if (response.error) {
-				console.error(response);
-			}
-		}
+		return new Response(JSON.stringify('OK'));
 	} catch (err) {
-		return new Response(JSON.stringify('err'));
+		return new Response(JSON.stringify('Error'));
 	}
-
-	return new Response(JSON.stringify('ok'));
 }
